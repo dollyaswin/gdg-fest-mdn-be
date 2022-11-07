7ad53b18ec6ceff6dafd9bdcd7163618c079c0ad
Dolly Aswin <dolly.aswin@gmail.com>
2022-11-07 23:32:45 +0700

    Add variable for current git branch

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,13 @@
 BE_PATH=/var/www/gdg/api/current
 
 echo "Build..."
+git pull origin ${ENV_BRANCH}
 # rm -fr vendor
 # composer install
 
 echo "Deploy..."
 rsync --exclude ".git" -av . $BE_PATH/.
 
+echo "Reload PHP & Nginx"
 sudo systemctl reload php8.1-fpm
 sudo systemctl reload nginx
