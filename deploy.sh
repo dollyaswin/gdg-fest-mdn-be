5ef56f9fd66148bea3e9d2d130befdfb3d4e5563
Dolly Aswin <dolly.aswin@gmail.com>
2022-11-10 23:51:17 +0700

    Add export environment on deployment

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "Read Configuration..."
-while read line; do export $line; done < .env
+#echo "Read Configuration..."
+#while read line; do export $line; done < .env
 
 echo "Build..."
-git pull origin ${ENV_BRANCH}
+git pull origin ${BRANCH}
 # rm -fr vendor
 # composer install
 
 echo "Deploy..."
-rsync --exclude ".git" -av . $BE_PATH/.
+rsync --exclude ".git" -av . $APP_PATH/.
 
 echo "Reload PHP & Nginx"
 sudo systemctl reload php8.1-fpm
