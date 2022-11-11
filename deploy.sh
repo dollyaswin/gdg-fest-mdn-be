54ce82dceb977675cd94f653102569dca3409d4f
Dolly Aswin <dolly.aswin@gmail.com>
2022-11-11 17:32:50 +0700

    Remove .env.dist and add --delete on rsync

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,11 @@
 #while read line; do export $line; done < .env
 
 echo "Build..."
-# git pull origin ${BRANCH}
 # rm -fr vendor
 # composer install
 
 echo "Deploy..."
-rsync --exclude ".git" -av . $APP_PATH/.
+rsync --exclude ".git" -av --delete . $APP_PATH/.
 
 echo "Reload PHP & Nginx"
 sudo systemctl reload php8.1-fpm
