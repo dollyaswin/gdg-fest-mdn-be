610b0fec2d9a1319ec119ce6cbb0b240164aa2d3
Dolly Aswin <dolly.aswin@gmail.com>
2022-11-11 21:47:42 +0700

    #2 Missing .env.example and run clear cache in deployment

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,15 @@ rm -fr vendor
 composer install
 
 echo "Deploy..."
-rsync --exclude ".git" -av --delete . $APP_PATH/.
+rsync --exclude '.git' --exclude '.env' --exclude 'storage' -av --delete . $APP_PATH/.
+
+cd $APP_PATH/
+
+# clear cache
+php artisan cache:clear
+
+# run database migration
+#php artisan migrate --force
 
 echo "Reload PHP & Nginx"
 sudo systemctl reload php8.1-fpm
