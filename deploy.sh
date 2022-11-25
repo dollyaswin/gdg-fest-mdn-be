70edb61cbfadbf37200150e49e5c0a84addc1731
Dolly Aswin <dolly.aswin@gmail.com>
2022-11-26 00:32:01 +0700

    #3 Run PHPUnit & add some comment

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
+# Continuous Integration
 echo "Build..."
 cd src
 rm -fr vendor
 composer install
 
+echo "Testing..."
+vendor/bin/phpunit
+
+# Continuous Deployment
 echo "Deploy..."
 rsync --exclude '.git' --exclude 'storage' -a --delete . $APP_PATH/.
 
 cd $APP_PATH/
 
-# cache
-echo "Clear Config/Cache..."
+echo "Clear Cache..."
 php artisan cache:clear
 
-# run database migration
 echo "Database Migration..."
 php artisan migrate --force
 
-echo "Reload PHP & Nginx"
+echo "Reload FPM & Nginx"
 sudo systemctl reload php8.1-fpm
 sudo systemctl reload nginx
